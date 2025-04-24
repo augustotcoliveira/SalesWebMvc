61310b62c192069fc2f6e81eefd93a9e3a9c6d6c
Augusto Toledo Caires de Oliveira <a199793@dac.unicamp.br>
2025-04-24 13:28:46 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 namespace SalesWebMvc.Models
 {
     public class Seller
@@ -6,9 +7,12 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate{ get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales{ get; set; } = new List<SalesRecord>();
 
         public Seller()
