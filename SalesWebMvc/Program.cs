cd1a5daf463f49aa0b7d4e037b9dae17c9a1660e
Augusto Toledo Caires de Oliveira <a199793@dac.unicamp.br>
2025-04-24 09:40:29 -0300

    adding development env and adding seeding service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -16,13 +16,33 @@ namespace SalesWebMvc
                     mySqlOptions =>
                         mySqlOptions.MigrationsAssembly("SalesWebMvc")));
 
+            builder.Services.AddScoped<SeedingService>(); //registra o servico para rodar uma vez por escopo
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                using (var scope = app.Services.CreateScope()) // Cria um escopo em Services para recuperar SeedingService
+                {
+
+                    try
+                    {
+                        var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>(); // Acessa os registros do escopo (service provider) e recupera a instancia de SeedingService
+                        seedingService.Seed();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Erro ao executar o seeding: {ex.Message}");
+                    }
+                }
+            }
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
